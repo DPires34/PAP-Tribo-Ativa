59e19a7efc4f69ed1379611b9b3bee9e3eac1824
DPires34 <chewiedave34@gmail.com>
2025-01-14 17:55:43 +0000

    Add files via upload

--- login-logic.php
+++ login-logic.php
@@ -3,7 +3,17 @@
 require_once('Files/funcao.php');
 
 $email = trim($_POST['email']);
+$password = trim($_POST['password']);
+$password_1 = trim($_POST['password_1']);
+$numero_telemovel = trim($_POST['numero_telemovel']);
+
+
 $sql = "SELECT * FROM utilizadores WHERE email = '{$email}'";
-die($sql);
+$res = $conn->query($sql);
+
+if($res->num_rows > 0){
+    die("Utilizador com o mesmo email já existente.");
+}
+
 echo "<pre>";
 print_r($_POST);
